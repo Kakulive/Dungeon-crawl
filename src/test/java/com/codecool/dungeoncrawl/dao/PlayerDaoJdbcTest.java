0af690574ac04134e46c8559207dc77a3ec53cc7
DDubicki <dubickidymitr@gmail.com>
2021-09-30 17:15:41 +0200

    PlayerDaoJdbcTest. Tests for all methods are implemented

--- src/test/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbcTest.java
+++ src/test/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbcTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -16,55 +17,37 @@ import static org.junit.jupiter.api.Assertions.*;
     environment variables for the test are also set
  */
 
-// TODO uncommented all
-
 class PlayerDaoJdbcTest {
     private GameDatabaseManager databaseManager;
     private PlayerDao playerDao;
+    PlayerModel firstPlayer = new PlayerModel("John", 10, 5, 15, 15, 5, false, "sword;shield;");
+    PlayerModel secondPlayer = new PlayerModel("Brad Pitt", 50, 10, 10, 30, 40, false, "sword;shield;key;");
 
-    // Commented during the test
-
-//    @BeforeEach
-//    public void clearDB() throws SQLException {
-//        databaseManager = new GameDatabaseManager();
-//        databaseManager.setup();
-//        playerDao = new PlayerDaoJdbc(databaseManager.connect());
-//        try (Connection connection = databaseManager.connect().getConnection()) {
-//            String sql = "TRUNCATE player CASCADE;";
-//            PreparedStatement statement = connection.prepareStatement(sql);
-//            statement.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error, cannot connect database", e);
-//        }
-//    }
-
-    // Just for the test
     @BeforeEach
-    private void connectDB() throws SQLException {
+    public void clearDB() throws SQLException {
         databaseManager = new GameDatabaseManager();
         databaseManager.setup();
         playerDao = new PlayerDaoJdbc(databaseManager.connect());
+        try (Connection connection = databaseManager.connect().getConnection()) {
+            String sql = "TRUNCATE player CASCADE;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error, cannot connect database", e);
+        }
     }
 
     @Test
     void add_whenAddingNewPlayerViaDao_addsPlayerCorrectlyAndSetItsId() {
         // given
-        PlayerModel player = new PlayerModel("John", 10, 5, 15, 15, 5, false, "sword;shield;");
         // when
-        playerDao.add(player);
+        playerDao.add(firstPlayer);
         // then
         List<PlayerModel> savedPlayers = databaseManager.getAllSavedPlayers();
         assertNotNull(savedPlayers);
-//        assertEquals(1, savedPlayers.size());  //
+        assertEquals(1, savedPlayers.size());
         PlayerModel testedPlayer = savedPlayers.get(savedPlayers.size() - 1);
-        assertEquals("John", testedPlayer.getPlayerName());
-        assertEquals(10, testedPlayer.getHp());
-        assertEquals(5, testedPlayer.getX());
-        assertEquals(15, testedPlayer.getY());
-        assertEquals(15, testedPlayer.getAttack());
-        assertEquals(5, testedPlayer.getArmor());
-        assertFalse(testedPlayer.getHasKey());
-        assertEquals("sword;shield;", testedPlayer.getItems());
+        checkAssertions(firstPlayer, testedPlayer);
         assertNotNull(testedPlayer.getId());
     }
 
@@ -82,30 +65,22 @@ class PlayerDaoJdbcTest {
     @Test
     void update_whenUpdatePlayerDataViaDao_dataUpdatesCorrectly() {
         // given
-        PlayerModel playerToAdd = new PlayerModel("John", 10, 5, 15, 15, 5, false, "sword;shield;");
-        playerDao.add(playerToAdd);
-        PlayerModel playerToUpdate = new PlayerModel("Brad Pitt", 50, 10, 10, 30, 40, true, "sword;shield;key;");
+        playerDao.add(firstPlayer);
         List<PlayerModel> savedPlayers = databaseManager.getAllSavedPlayers();
         int playerId = savedPlayers.get(savedPlayers.size() - 1).getId();
         // when
-        playerDao.update(playerToUpdate, playerId);
+        playerDao.update(secondPlayer, playerId);
         // then
         List<PlayerModel> updatedPlayer = databaseManager.getAllSavedPlayers();
         PlayerModel testedPlayer = updatedPlayer.get(updatedPlayer.size() - 1);
-        assertEquals("Brad Pitt", testedPlayer.getPlayerName());
-        assertEquals(50, testedPlayer.getHp());
-        assertEquals(10, testedPlayer.getX());
-        assertEquals(10, testedPlayer.getY());
-        assertEquals(30, testedPlayer.getAttack());
-        assertEquals(40, testedPlayer.getArmor());
-        assertTrue(testedPlayer.getHasKey());
-        assertEquals("sword;shield;key;", testedPlayer.getItems());
+        checkAssertions(secondPlayer, testedPlayer);
         assertEquals(playerId, testedPlayer.getId());
     }
 
     @Test
     void whenUpdatePlayerDataWithPlayerThatIsNull_ThrowsNullPointerException() {
         // given
+        playerDao.add(firstPlayer);
         PlayerModel player = null;
         List<PlayerModel> savedPlayers = databaseManager.getAllSavedPlayers();
         int playerId = savedPlayers.get(savedPlayers.size() - 1).getId();
@@ -119,22 +94,14 @@ class PlayerDaoJdbcTest {
     @Test
     void get_whenGetPlayerByIdViaDao_ReturnRequiredPlayerCorrectly() {
         // given
-        PlayerModel playerToAdd = new PlayerModel("John", 10, 5, 15, 15, 5, false, "sword;shield;");
-        playerDao.add(playerToAdd);
+        playerDao.add(firstPlayer);
         List<PlayerModel> savedPlayers = databaseManager.getAllSavedPlayers();
         int playerId = savedPlayers.get(savedPlayers.size() - 1).getId();
         // when
         PlayerModel testedPlayer = playerDao.get(playerId);
         // then
         assertNotNull(testedPlayer);
-        assertEquals("John", testedPlayer.getPlayerName());
-        assertEquals(10, testedPlayer.getHp());
-        assertEquals(5, testedPlayer.getX());
-        assertEquals(15, testedPlayer.getY());
-        assertEquals(15, testedPlayer.getAttack());
-        assertEquals(5, testedPlayer.getArmor());
-        assertFalse(testedPlayer.getHasKey());
-        assertEquals("sword;shield;", testedPlayer.getItems());
+        checkAssertions(firstPlayer, testedPlayer);
     }
 
     @Test
@@ -150,14 +117,25 @@ class PlayerDaoJdbcTest {
     @Test
     void getAll_WhenGetAllPlayersViaDao_ReturnListOfAllPlayersCorrectly() {
         // given
-        PlayerModel firstPlayer = new PlayerModel("John", 10, 5, 15, 15, 5, false, "sword;shield;");
-        PlayerModel secondPlayer = new PlayerModel("Brad Pitt", 50, 10, 10, 30, 40, true, "sword;shield;key;");
         playerDao.add(firstPlayer);
         playerDao.add(secondPlayer);
         // when
         List<PlayerModel> savedPlayers = databaseManager.getAllSavedPlayers();
         // then
         assertNotNull(savedPlayers);
-//        assertEquals(2, savedPlayers.size());
+        assertEquals(2, savedPlayers.size());
+        checkAssertions(firstPlayer, savedPlayers.get(0));
+        checkAssertions(secondPlayer, savedPlayers.get(1));
+    }
+
+    private void checkAssertions(PlayerModel expectedPlayer, PlayerModel testedPlayer) {
+        assertEquals(expectedPlayer.getPlayerName(), testedPlayer.getPlayerName());
+        assertEquals(expectedPlayer.getHp(), testedPlayer.getHp());
+        assertEquals(expectedPlayer.getX(), testedPlayer.getX());
+        assertEquals(expectedPlayer.getY(), testedPlayer.getY());
+        assertEquals(expectedPlayer.getAttack(), testedPlayer.getAttack());
+        assertEquals(expectedPlayer.getArmor(), testedPlayer.getArmor());
+        assertFalse(testedPlayer.getHasKey());
+        assertEquals(expectedPlayer.getItems(), testedPlayer.getItems());
     }
 }
